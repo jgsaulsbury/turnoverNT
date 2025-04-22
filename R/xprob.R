5642856887119aa3fa5a1d6a13be0cc319507f23
James Saulsbury <jgsaulsbury@gmail.com>
2025-04-22 12:25:38 -0500

    modified xprob

--- R/xprob.R
+++ R/xprob.R
@@ -13,7 +13,8 @@
 #' @param m vector of relative abundances at start of transition.
 #' @param n vector of relative abundances at end of transition.
 #' @param Jt ratio of J (community size) to t (# generations).
-#' @param ss vector of length 2 giving number of samples at times for m and n.
+#' @param ss single value or vector of length 2 giving number of samples at
+#' times for m and n. If a single value given, assumes it applies to both m and n.
 #'
 #' @importFrom cbinom dcbinom
 #'
@@ -21,7 +22,7 @@
 #' @export
 #'
 #' @examples
-#'xprob(m=c(0.1,0.1,0.8),n=c(0.2,0.3,0.5),Jt=10,ss=c(1000,1000))
+#'xprob(m=c(0.1,0.1,0.8),n=c(0.2,0.3,0.5),Jt=10,ss=c(1000,1000)) #-0.01480662
 #'
 xprob <- function(m,n,Jt,ss=NA){
   #error handling
@@ -32,16 +33,20 @@ xprob <- function(m,n,Jt,ss=NA){
   if(!Jt>0){stop("Jt must be positive")}
   #body
   if(!any(is.na(ss))){ #if ss given...
+    if(length(ss)==1){ss <- rep(ss,2)} #duplicate if a single value given
     sizes <- c(ss,Jt)
     size <- min(sizes) / sum(min(sizes)/sizes)
   } else {
     size <- Jt}
-  while(sum(m)==1 | sum(n)==1){ #remove last species if sum is 1, not needed for calculation
+  order <- rev(order(m)) #sort by abundance of m
+  m <- m[order]
+  n <- n[order]
+  while(sum(m) > 1-tol | sum(n) > 1-tol){ #remove last species if sum is 1, not needed for calculation
     m <- m[-length(m)]
     n <- n[-length(n)]}
+  m <- m[!n==0];n <- n[!n==0] #remove indices where n is zero (zeros in m removed in previous step)
   n <- n*size + 0.5 #moving onto scale of cbinom
-  out <- ifelse(m[1]>0 & n[1]>0,cbinom::dcbinom(x=n[1],size=size,prob=m[1],log=T)+log(size+1),0) #for sp 1 (multiplied to make sense for prob densities)
-  for(i in seq_len(length(m))[-1]){ #for every other sp i
-    if(m[i]>0 & n[i]>0){ #if sp i not at abundance 0...
-      out <- out + dcbinom(x=n[i],size=size-sum(n[1:i-1]-0.5),prob=m[i]/(1-sum(m[1:i-1])),log=T)+log(size+1)}}
+  out <- cbinom::dcbinom(x=n[1],size=size,prob=m[1],log=T)+log(size+1) #for taxon 1 (multiplied to make sense for prob densities)
+  for(i in seq_len(length(m))[-1]){ #for every other taxon i
+      out <- out + dcbinom(x=n[i],size=size-sum(n[1:i-1]-0.5),prob=m[i]/(1-sum(m[1:i-1])),log=T)+log(size+1)}
   return(out)}
