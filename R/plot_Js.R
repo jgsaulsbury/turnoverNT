f23874a16752d82e91aa7768e1e24c2b11e46c8d
James Saulsbury <jgsaulsbury@gmail.com>
2025-05-17 18:57:54 -0500

    fixed problem with plot_Js and updated example

--- R/plot_Js.R
+++ R/plot_Js.R
@@ -21,14 +21,23 @@
 #' @export
 #'
 #' @examples
-#' J <- 10000
+#' #simulate a timeseries with an increase in J halfway through
+#' J1 <- 1000 #first half of TS
+#' J2 <- 10000 #second half of TS
 #' tslength <- 500
 #' every <- 5
 #' nsp <- 8
+#' ss <- 1000000
 #' ages <- seq(0,tslength,every)
-#' timeseries <- simNT(startingabs=rep(J/nsp,nsp),ts=ages,ss=100000)
-#' plot_Js(timeseries$simulation,timeseries$times,linesevery = 100)
-plot_Js <- function(occs,ages,xlim=NULL,linesevery=NA,generationtime=1,searchinterval=c(1,9),...){
+#' timeseries1 <- simNT(startingabs=rep(J1/nsp,nsp),ts=ages[1:(length(ages)/2 + 1)],ss=ss)$simulation #first half
+#' timeseries2 <- simNT(startingabs=timeseries1[1,]*J2/ss,ts=ages[1:(length(ages)/2 + 1)],ss=ss)$simulation #second half
+#' timeseries <- rbind(timeseries2,timeseries1[-1,])
+#' par(mfrow=c(1,2))
+#' plot_spindles(timeseries,ages,plot.ss=FALSE)
+#' lines(c(-1,10),c(tslength/2,tslength/2),lty='dashed',lwd=2) #plot point where J switches
+#' plot_Js(timeseries,ages) #fit rate through time
+#' lines(c(1E-10,1E-1),c(tslength/2,tslength/2),lty='dashed',lwd=2)
+plot_Js <- function(occs,ages,xlim=NULL,linesevery=NA,sampled=TRUE,generationtime=1,searchinterval=c(1,9),...){
   if(is.list(occs)){
     occs <- as.matrix(occs)}
   xages <- (head(ages,-1) + tail(ages,-1))/2 #midpoint of each transition
@@ -37,7 +46,7 @@ plot_Js <- function(occs,ages,xlim=NULL,linesevery=NA,generationtime=1,searchint
   JUBs <- c() #upper bounds on J
   for(i in seq(length(ages)-1)){ #for every transition
     transition <- occs[(length(ages)-i):(length(ages)-i+1),]
-    fit <- fitJ(occs=transition,ages=ages[i:(i+1)],sampled=TRUE,generationtime=generationtime,CI=TRUE,searchinterval=searchinterval)
+    fit <- fitJ(occs=transition,ages=ages[i:(i+1)],sampled=sampled,generationtime=generationtime,CI=TRUE,searchinterval=searchinterval)
     Jhats <- c(Jhats,fit$J)
     JLBs <- c(JLBs,fit$CI[1])
     JUBs <- c(JUBs,fit$CI[2])}
@@ -48,13 +57,11 @@ plot_Js <- function(occs,ages,xlim=NULL,linesevery=NA,generationtime=1,searchint
   if(!is.na(linesevery)){#horizontal lines depicting time
     for(t in seq(0,max(ages)+linesevery,linesevery)){
       graphics::lines(c(xlim[1],xlim[2]),c(t,t),col="grey90")}}
-  points(1/Jhats,xages)
+  points(1/Jhats,rev(xages))
   for(i in seq(length(xages))){ #for every transition
-    graphics::lines(c(1/JLBs[i],1/JUBs[i]),c(xages[i],xages[i]))}
+    graphics::lines(c(1/JLBs[i],1/JUBs[i]),c(rev(xages)[i],rev(xages)[i]))} #draw error bars
   axis(side=1,at=10^seq(floor(log10(xlim[1])),ceiling(log10(xlim[2])))) #tick marks
-  #axis(side=1,at=c(seq(1E3,1E4,1E3),seq(1E4,1E5,1E4),seq(1E5,1E6,1E5)),labels = FALSE,tck=-0.01)
   for(i in seq(floor(log10(xlim[1])),floor(log10(xlim[2])))){
     axis(side=1,at=c(seq(10^i,10^(i+1),10^i)),labels = FALSE,tck=-0.01)
   }
-
 }
